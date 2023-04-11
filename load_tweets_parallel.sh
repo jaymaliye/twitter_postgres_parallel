cb8105d6a61b2a8e6144655a3c36b2768705a2e4
Jay Maliye <jaymaliye@gmail.com>
2023-04-11 16:43:04 -0700

    third commit

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -11,9 +11,9 @@ echo "$files" | time parallel sh load_denormalized.sh
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:2/ --inputs
+echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:6767/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs
+echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:8726/ --inputs
